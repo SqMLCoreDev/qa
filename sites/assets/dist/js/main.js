f7572408827aa6ea6c193ef3b3c641bb2ab5b334
Naveen <naveenozzzo@gmail.com>
2021-04-28 15:02:31 +0530

    Update main.js

--- sites/assets/dist/js/main.js
+++ sites/assets/dist/js/main.js
@@ -511,6 +511,7 @@ var payee = async function payButton(data) {
 		"paymentDescription": description
 	};
 	console.log(json, data.isMember);
+	alert(JSON.stringify(json));
 	console.log('flutterPlatformReady', window.platformReady);
 	//alert('window flutterPlatformReady', platformReady);
 	let transaction = null;
