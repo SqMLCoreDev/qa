9f97a1ea914d26f2e048ca9b4a903b19c0508c75
javadeveloperteam <46495129+javadeveloperteam@users.noreply.github.com>
2022-07-21 18:06:57 +0530

    Update main.js

--- sites/assets/dist/js/main.js
+++ sites/assets/dist/js/main.js
@@ -333,9 +333,12 @@ function membershipCard(formData, scheme) {
 		card += "</div>";
 		card += "<div class='plan-details u-d-flex u-d-flex--center u-border-bottom--paleGrey'>";
 		card += "<div class='u-p-b--20 u-p-t--8'>";
+		if(data.clientId=="DDA"){
+		}else{
 		card += "<div class='text-gre u-text--striked text-epsilon u-m-r--4 il-f'>";
 		card += "<div class='text-currency-symbol'>" + data.schemeCurrencySymbol + "</div>";
 		card += "<div class=''>" + data.schemeActualPrice + "</div>";
+		}
 		card += "</div>";
 		card += "<div class='card_container'>";
 		card += "<sup class='text-currency-symbol'>" + data.schemeCurrencySymbol + "</sup><span class='text-delta text-currency text-center u-text--bold u-align--vm cardBlock'>" + data.schemeActivePrice + "</span>";
